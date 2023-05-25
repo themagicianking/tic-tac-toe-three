705acdd4b135121e302621c786282ace7a67031f
themagicianking <tmoonder@gmail.com>
2023-05-25 16:07:01 -0400

    Added logic for the player's turn.

--- main.rb
+++ main.rb
@@ -1,25 +1,21 @@
 module Board
   def display(data)
     # data should be an array used to print out the board
-    puts " 1 | 2 | 3 "
+    puts " #{data[:a]} | #{data[:b]} | #{data[:c]} "
     puts " -   -   -"
-    puts " 4 | 5 | 6 "
+    puts " #{data[:d]} | #{data[:e]} | #{data[:f]} "
     puts " -   -   -"
-    puts " 7 | 8 | 9"
+    puts " #{data[:g]} | #{data[:h]} | #{data[:i]}"
   end
 end
 
 class Player
-  include Board
-
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
   end
 
-  def turn
-    puts # prints the board out, mixin from a module?
-  end
+  attr_reader :name, :symbol
 
   def check_win
     # check all the conditions that the board can have that would mean a win
@@ -27,7 +23,37 @@ class Player
   end
 end
 
-player_one = Player.new("thane", "x")
+class Game
+  include Board
 
-puts player_one
-player_one.display("meep")
+  def initialize(player_one, player_two)
+    @player_one = player_one
+    @player_two = player_two
+    @board_data = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9}
+  end
+
+  def board_data
+    return @board_data
+  end
+
+  def turn(name, symbol)
+    puts "Hello #{name}, where would you like to place your symbol?"
+    choice = gets.to_i
+    change = 0
+    board_data.each do |key, value| 
+      if choice == value
+        change = key
+      end
+    end
+    board_data[change] = symbol
+    self.display(board_data)
+  end
+end
+
+player_one = Player.new("thane", "x")
+player_two = Player.new("turkey", "o")
+
+new_game = Game.new(player_one, player_two)
+
+puts new_game.display(new_game.board_data)
+new_game.turn(player_one.name, player_one.symbol)
